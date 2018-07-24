49c5ea67338a3caf523f323152a79824c38126a7
jbickford-smith <jbickford-smith@decenture.com>
2018-07-24 15:22:02 +0100

    first commit

---
+++ tutorial-network2/README.md
@@ -0,0 +1,3 @@
+# tutorial-network
+
+dev_tutorial
---
+++ tutorial-network2/lib/script.js
@@ -0,0 +1,18 @@
+/**
+ * Sample transaction processor function.
+ * @param {org.example.basic.Mint} tx The sample transaction instance.
+ * @transaction
+ */
+async function Mint(tx) {  // eslint-disable-line no-unused-vars
+
+    // Save the old value of the asset.
+    const oldValue = tx.asset.value;
+	
+    // Update the asset with the new value.
+    tx.asset.value = oldValue + tx.newValue;
+
+    // Get the asset registry for the asset.
+    const assetRegistry = await getAssetRegistry('org.example.basic.SampleAsset');
+    // Update the asset in the asset registry.
+    await assetRegistry.update(tx.asset);
+}
